6713f278a2d1ba5ba2346789fe7b9b11a93e4cfa
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-12 12:46:13 +0300

    conditional rendering added for products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -40,13 +40,22 @@ const Products = () => {
 	return (
 		<div className="container mx-auto px-4 py-8">
 			<Filter />
-			<h2 className="text-3xl font-bold text-gray-800 mb-6">Our Products</h2>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-				{products &&
-					products.map((product) => (
-						<ProductCard key={product.id} {...product} />
-					))}
-			</div>
+			{products?.length > 0 ? (
+				<>
+					<h2 className="text-3xl font-bold text-gray-800 mb-6">
+						Our Products
+					</h2>
+					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+						{products.map((product) => (
+							<ProductCard key={product.id} {...product} />
+						))}
+					</div>
+				</>
+			) : (
+				<div className="flex items-center justify-center py-8">
+					<p className="text-xl text-red-600">No products found!</p>
+				</div>
+			)}
 		</div>
 	);
 };
