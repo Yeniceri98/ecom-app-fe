8deb1ce7b30f393f9a4cdd4a2db4773508a8716a
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-13 11:15:29 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './components/Products';
+import Home from './components/Home';
 
 function App() {
 	return (
-		<>
-			<Products />
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/products" element={<Products />} />
+			</Routes>
+		</Router>
 	);
 }
 
