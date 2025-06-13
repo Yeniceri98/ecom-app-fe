590dc675d4538a7197b54a170d8b80b94ecb71dd
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-13 11:08:42 +0300

    pagination range update

--- src/components/shared/Paginations.jsx
+++ src/components/shared/Paginations.jsx
@@ -56,7 +56,10 @@ const Paginations = ({ paginationObject }) => {
 						<MenuItem value={1}>1</MenuItem>
 						<MenuItem value={2}>2</MenuItem>
 						<MenuItem value={3}>3</MenuItem>
-						<MenuItem value={4}>4</MenuItem>
+						<MenuItem value={5}>5</MenuItem>
+						<MenuItem value={5}>10</MenuItem>
+						<MenuItem value={5}>15</MenuItem>
+						<MenuItem value={5}>20</MenuItem>
 					</Select>
 				</FormControl>
 			</Box>
