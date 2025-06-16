799809902f70028901bbff54e3e3dc7fdce750b0
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-16 13:03:34 +0300

    DrawerItem styling update & Cart and Login added

--- src/components/shared/DrawerItem.jsx
+++ src/components/shared/DrawerItem.jsx
@@ -13,14 +13,17 @@ import {
 	Toolbar,
 	Typography,
 	useTheme,
+	Badge,
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import HomeIcon from '@mui/icons-material/Home';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import GroupsIcon from '@mui/icons-material/Groups';
-import { Link } from 'react-router-dom';
+import LoginIcon from '@mui/icons-material/Login';
+import StoreIcon from '@mui/icons-material/Store';
 
 const drawerWidth = 200;
 
@@ -65,9 +68,26 @@ function DrawerItem({ children }) {
 							sx={{ mr: 2, ...(open && { display: 'none' }) }}>
 							<MenuIcon />
 						</IconButton>
-						<Typography variant="h6" noWrap component="div">
-							ASY ECOM
-						</Typography>
+						<IconButton color="inherit" component={Link} to="/">
+							<StoreIcon className="mr-1" />
+							<Typography variant="h6" noWrap component="div">
+								ASY ECOM
+							</Typography>
+						</IconButton>
+					</Box>
+					<Box sx={{ display: 'flex', gap: 2 }}>
+						<IconButton color="inherit" component={Link} to="/cart">
+							<Badge badgeContent="0" color="error">
+								<ShoppingBasketIcon />
+							</Badge>
+							<Typography variant="subtitle2" sx={{ ml: 1 }}>
+								Cart
+							</Typography>
+						</IconButton>
+						<IconButton color="inherit" component={Link} to="/login">
+							<LoginIcon className="mr-1" />
+							<Typography variant="subtitle2">Login</Typography>
+						</IconButton>
 					</Box>
 				</Toolbar>
 			</MuiAppBar>
