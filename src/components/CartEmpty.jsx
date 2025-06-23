a949d5bf0bffefdcb22fca961c40a176a0fcddb7
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-23 09:51:17 +0300

    shopping basket added to CartEmpty

--- src/components/CartEmpty.jsx
+++ src/components/CartEmpty.jsx
@@ -1,6 +1,9 @@
+import { SlBasket } from 'react-icons/sl';
+
 const CartEmpty = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-[400px] gap-6">
+			<SlBasket size={64} className="text-gray-400" />
 			<h2 className="text-2xl font-semibold text-gray-600">
 				Your cart is empty
 			</h2>
