df1d99631c4cc3856a718d16082447376fdd8207
Ahmet Sami Yeniceri <ahmet.sami.yeniceri@ericsson.com>
2025-06-30 13:49:06 +0300

    withCredentials parameter added to allow cookies from frontend

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,7 @@ const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
 	baseURL: `${BASE_URL}/api`,
+	withCredentials: true, // It allows cookies to be sent with the request sent from the browser.
 });
 
 export default api;
